d2845b065b74cb4886e31f8911a46d85d09b21ca
HUYNH Henry <henry018@laposte.net>
2023-06-17 18:14:42 +0200

    [android] canStopLocationService workaround

--- geolocator_android/android/src/main/java/com/baseflow/geolocator/GeolocatorLocationService.java
+++ geolocator_android/android/src/main/java/com/baseflow/geolocator/GeolocatorLocationService.java
@@ -88,7 +88,7 @@ public class GeolocatorLocationService extends Service {
     if(cancellationRequested) {
        return listenerCount == 1;
     }
-    return connectedEngines == 0;
+    return connectedEngines >= 0;
   }
   public void flutterEngineConnected() {
     connectedEngines++;
